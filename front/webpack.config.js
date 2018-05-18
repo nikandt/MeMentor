575d64e067a18fa0b26b6a7b2fd14f151459814f
wyozi <joonas.mertanen@gmail.com>
2018-05-18 21:46:06 +0800

    Add frontend

---
+++ front/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "public"),
+        compress: true,
+        port: 9000
+    }
+};
