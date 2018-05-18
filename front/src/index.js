eaeb873c1b1272d914b762e8c494d8ecef81847a
Tessa Nikander <tessa.nikander@aalto.fi>
2018-05-18 23:23:47 +0800

    Added value for society

--- front/src/index.js
+++ front/src/index.js
@@ -2,42 +2,56 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Button from '@material-ui/core/Button';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
-import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
-import RestoreIcon from '@material-ui/icons/Restore';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import HomeIcon from '@material-ui/icons/Home';
+import MessageIcon from '@material-ui/icons/Message';
+import SettingsIcon from '@material-ui/icons/Settings';
+import Settings from './settings';
 
 class App extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            page: "index"
-        };
-    }
+  constructor() {
+    super();
+    this.state = {
+      page: 'index'
+    };
+  }
 
-    renderPage() {
-        if (this.state.page === 'index') {
-            return (
-                <Button onClick={() => this.setState({ page: "otherpage" })}>
-                    Go to other page
-                </Button>
-            );
-        } else {
-            return (
-                <Button onClick={() => this.setState({ page: "index" })}>
-                    Back to index
-                </Button>
-            );
-        }
+  renderPage() {
+    if (this.state.page === 'index') {
+      return <div>THIS IS MAIN PAGE</div>;
+    } else {
+      return <Settings />;
     }
+  }
 
-    render() {
-        return <div>
-            { this.renderPage() }
-            <BottomNavigation>
-                <BottomNavigationAction label="aaaa" icon={<RestoreIcon />}/>
-                <BottomNavigationAction label="bbbb" icon={<RestoreIcon />}/>
-            </BottomNavigation>
-        </div>
-    }
+  switchPage(value) {
+    this.setState({ page: value });
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderPage()}
+        <BottomNavigation onChange={(event, value) => this.switchPage(value)}>
+          <BottomNavigationAction
+            value="otherpage"
+            label="aaaa"
+            icon={<HomeIcon />}
+          />
+          <BottomNavigationAction
+            value="otherpage"
+            label="bbbb"
+            icon={<MessageIcon />}
+          />
+          <BottomNavigationAction
+            value="otherpage"
+            label="cccc"
+            icon={<SettingsIcon />}
+          />
+        </BottomNavigation>
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('#app'));
---
+++ front/src/settings.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
+import RestoreIcon from '@material-ui/icons/Restore';
+
+class Settings extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return <div>
+      moioimoimoi
+        </div>
+  }
+}
+
+export default Settings
