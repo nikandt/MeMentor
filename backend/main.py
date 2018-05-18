331cb041a558786e65c27a8e51bb97c85ebe6a6a
Visa Pollari <visa@smartly.io>
2018-05-18 23:35:06 +0800

    working backend

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,17 @@
 from flask import Flask
-app = Flask(__name__)
+from server import server
 
+app = Flask(__name__)
+S = server()
 @app.route("/")
 def hello():
   return "Hello World!"
+
+@app.route("/add/<name>")
+def add(name):
+  return S.addUser(name)
+
+
+@app.route("/getUsers")
+def getUsers():
+  return S.getUsers()
--- backend/server.py
+++ backend/server.py
@@ -1,5 +1,24 @@
-import pymongo
+from pymongo import MongoClient
 
 class server:
   def __init__(self):
-    self.port = 4999
+    self.port = 27017
+    #password
+    self.client = MongoClient("mongodb+srv://jebujebu:jebujebunsalasanaananas@junctiontsinghua-rfzol.mongodb.net/test")
+    self.db = self.client['test']
+    self.userCollection = self.db["user_collection"]
+
+  def addUser(self, name):
+    post = {"author": name,}
+
+    print( self.userCollection.posts.insert_one(post).inserted_id )
+    return "jee"
+
+  def getUsers(self):
+    ret = []
+    cursor = self.userCollection.posts.find()
+    print(cursor)
+    for user in cursor:
+      print(user)
+      ret.append( user['author'] )
+    return ", ".join(ret)
