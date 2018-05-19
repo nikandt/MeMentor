4fbb6142d53241d8b23f6e1b4bf86744312efc88
Tessa Nikander <tessa.nikander@aalto.fi>
2018-05-19 11:23:06 +0800

    Toolbars

--- front/src/message.js
+++ front/src/message.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import Button from '@material-ui/core/Button';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import RestoreIcon from '@material-ui/icons/Restore';
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
+import Toolbar from '@material-ui/core/Toolbar';
 
 class Message extends React.Component {
   constructor() {
@@ -13,6 +15,13 @@ class Message extends React.Component {
   render() {
     return (
       <div>
+        <AppBar position="static" color="default">
+          <Toolbar>
+            <Typography variant="title" color="inherit">
+              Messages
+            </Typography>
+          </Toolbar>
+        </AppBar>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet assumenda
         consectetur debitis magnam nobis possimus velit? Atque cumque, debitis
         dolores, dolorum error inventore quia quidem repudiandae tempora vitae
--- front/src/settings.js
+++ front/src/settings.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import Button from '@material-ui/core/Button';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import RestoreIcon from '@material-ui/icons/Restore';
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
+import Toolbar from '@material-ui/core/Toolbar';
 
 class Settings extends React.Component {
   constructor() {
@@ -11,7 +13,18 @@ class Settings extends React.Component {
   }
 
   render() {
-    return <div>THIS IS SETTINGS</div>;
+    return (
+      <div>
+        <AppBar position="static" color="default">
+          <Toolbar>
+            <Typography variant="title" color="inherit">
+              Settings
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        THIS IS SETTINGS
+      </div>
+    );
   }
 }
 
