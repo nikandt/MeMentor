e862df2b1edcefaee35010f2fffa55984c36b160
Tessa Nikander <tessa.nikander@aalto.fi>
2018-05-19 16:42:22 +0800

    Update layout

--- front/src/settings.js
+++ front/src/settings.js
@@ -19,7 +19,9 @@ import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
 import Visibility from '@material-ui/icons/Visibility';
 import VisibilityOff from '@material-ui/icons/VisibilityOff';
-import Divider from '@material-ui/core/Divider';
+
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
@@ -33,6 +35,7 @@ const styles = theme => ({
     display: 'flex',
     flexWrap: 'wrap'
   },
+
   textField: {
     marginLeft: theme.spacing.unit,
     marginRight: theme.spacing.unit,
@@ -46,7 +49,22 @@ const styles = theme => ({
   },
   chip: {
     margin: theme.spacing.unit
-  }
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+
+    color: theme.palette.text.secondary
+  } /*,
+  img: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    width: 500,
+    height: 500,
+    marginTop: '-50%',
+    marginLeft: '-50%'
+  }*/
 });
 
 function handleClick() {
@@ -78,9 +96,13 @@ function handleDelete(userid) {
     });*/
 }
 
+function getUser() {}
+
 class Settings extends React.Component {
   constructor() {
     super();
+
+    getUser();
 
     this.state = {
       email: '',
@@ -119,104 +141,126 @@ class Settings extends React.Component {
             </Typography>
           </Toolbar>
         </AppBar>
-        <div className="">
-          <br />
-          <Typography variant="title" gutterBottom>
-            User settings
-          </Typography>
-          <Avatar alt={this.state.username} src={this.state.imageurl} />
-          <input
-            accept="image/*"
-            className={classes.input}
-            id="button-file"
-            multiple
-            type="file"
-          />
-          <label htmlFor="button-file">
-            <Button variant="raised" component="span" className="photos">
-              Upload
-            </Button>
-          </label>
 
-          <FormControl
-            className={classNames(classes.margin, classes.textField)}
-          >
-            <TextField
-              label="Username"
-              id="margin-none"
-              defaultValue="John"
-              className="container"
-              /* onChange={handleChange('username')}*/
-            />
-            <TextField
-              label="E-mail"
-              id="margin-none"
-              defaultValue="john@doe.com"
-              className="container"
-            />
-          </FormControl>
-          <FormControl
-            className={classNames(classes.margin, classes.textField)}
-          >
-            <InputLabel htmlFor="password">Password</InputLabel>
-            <Input
-              id="password"
-              type={this.state.showPassword ? 'text' : 'password'}
-              value={this.state.password}
-              onChange={this.handleChange('password')}
-              endAdornment={
-                <InputAdornment position="end">
-                  <IconButton
-                    aria-label="Toggle password visibility"
-                    onClick={this.handleClickShowPassword}
-                    onMouseDown={this.handleMouseDownPassword}
-                  >
-                    {this.state.showPassword ? (
-                      <VisibilityOff />
-                    ) : (
-                      <Visibility />
-                    )}
-                  </IconButton>
-                </InputAdornment>
-              }
-            />
-          </FormControl>
+        <div>
+          <Paper className={classes.paper}>
+            <Typography variant="title" gutterBottom>
+              User settings
+            </Typography>
 
-          <br />
-          <Typography variant="title" gutterBottom>
-            Skills you know
-          </Typography>
-          <Chip
-            label="Gardening"
-            onClick={handleClick}
-            onDelete={handleDelete}
-            className={classes.chip}
-          />
-          <br />
-          <InputLabel htmlFor="input-with-icon-adornment">Add skill</InputLabel>
-          <Input id="input-with-icon-adornment2" />
-          <br />
-          <br />
-          <Typography variant="title" gutterBottom>
-            Interests
-          </Typography>
-          <Chip
-            label="Scala"
-            onClick={handleClick}
-            onDelete={handleDelete}
-            className={classes.chip}
-          />
-          <Chip
-            label="Python"
-            onClick={handleClick}
-            onDelete={handleDelete}
-            className={classes.chip}
-          />
-          <br />
-          <InputLabel htmlFor="input-with-icon-adornment">
-            Add interest
-          </InputLabel>
-          <Input id="input-with-icon-adornment" />
+            <Grid container spacing={24}>
+              <Grid
+                container
+                item
+                xs={12}
+                sm={12}
+                justify="center"
+                alignItems="center"
+              >
+                <Avatar alt={this.state.username} src={this.state.imageurl} />
+              </Grid>
+              <Grid item xs={12} sm={12}>
+                <input
+                  accept="image/*"
+                  className={classes.input}
+                  id="button-file"
+                  multiple
+                  type="file"
+                />
+                <label htmlFor="button-file">
+                  <Button variant="raised" component="span" className="photos">
+                    Change photo
+                  </Button>
+                </label>
+              </Grid>
+
+              <Grid item xs={12} sm={12}>
+                <FormControl
+                  className={classNames(classes.margin, classes.textField)}
+                >
+                  <TextField
+                    label="Username"
+                    id="margin-none"
+                    defaultValue="John"
+                    className="container"
+                    /* onChange={handleChange('username')}*/
+                  />
+                  <TextField
+                    label="E-mail"
+                    id="margin-none"
+                    defaultValue="john@doe.com"
+                    className="container"
+                  />
+                </FormControl>
+                <FormControl
+                  className={classNames(classes.margin, classes.textField)}
+                >
+                  <InputLabel htmlFor="password">Password</InputLabel>
+                  <Input
+                    id="password"
+                    type={this.state.showPassword ? 'text' : 'password'}
+                    value={this.state.password}
+                    onChange={this.handleChange('password')}
+                    endAdornment={
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="Toggle password visibility"
+                          onClick={this.handleClickShowPassword}
+                          onMouseDown={this.handleMouseDownPassword}
+                        >
+                          {this.state.showPassword ? (
+                            <VisibilityOff />
+                          ) : (
+                            <Visibility />
+                          )}
+                        </IconButton>
+                      </InputAdornment>
+                    }
+                  />
+                </FormControl>
+              </Grid>
+            </Grid>
+          </Paper>
+
+          <Paper className={classes.paper}>
+            <Typography variant="title" gutterBottom>
+              Skills you know
+            </Typography>
+            <Chip
+              label="Gardening"
+              onClick={handleClick}
+              onDelete={handleDelete}
+              className={classes.chip}
+            />
+            <br />
+            <InputLabel htmlFor="input-with-icon-adornment">
+              Add skill
+            </InputLabel>
+            <Input id="input-with-icon-adornment2" />
+          </Paper>
+
+          <Paper className={classes.paper}>
+            <Typography variant="title" gutterBottom>
+              Interests
+            </Typography>
+            <Chip
+              label="Scala"
+              onClick={handleClick}
+              onDelete={handleDelete}
+              className={classes.chip}
+            />
+            <Chip
+              label="Python"
+              onClick={handleClick}
+              onDelete={handleDelete}
+              className={classes.chip}
+            />
+            <br />
+            <InputLabel htmlFor="input-with-icon-adornment">
+              Add interest
+            </InputLabel>
+            <Input id="input-with-icon-adornment" />
+          </Paper>
         </div>
       </div>
     );
