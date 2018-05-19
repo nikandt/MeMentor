601b86604525ac5412ad67d26ead3f736c2fc413
Visa Pollari <visa@smartly.io>
2018-05-19 12:50:16 +0800

    add fields

--- backend/user.py
+++ backend/user.py
@@ -3,7 +3,14 @@
 class user:
 
   def __init__(self):
-    self.checks = {"name": self.nameCheck, "skills": self.skillCheck, "interests": self.skillCheck}
+    self.checks = {
+      "name": self.nameCheck,
+      "email": self.emailCheck,
+      "coords": self.coordinateCheck,
+      "imageURL": self.nameCheck,
+      "skills": self.skillCheck,
+      "interests": self.skillCheck,
+    }
 
   def checkDict(self, entry):
     print(entry, self.checks)
@@ -12,6 +19,25 @@ class user:
   def nameCheck(self, name):
     return isinstance(name, str)
 
+  def emailCheck(self, email):
+    return isinstance(email, str) and "@" in email and "." in email
+
+  def coordinateCheck(self, coords):
+    return (not isinstance(coords, str)) and all( isinstance(coord, float) for coord in coords ) and len(coords) == 2
+
   def skillCheck(self, skills):
     return (not isinstance(skills, str)) and all( isinstance(skill, str) for skill in skills )
 
+
+
+#example
+"""
+{
+	"name": "DoubleJebu",
+	"email": "jee.puu@jebu.com",
+	"coords": [22.45, 5.5],
+	"imageURL": "http://via.placeholder.com/256x256",
+	"skills": ["C++", "Scala"],
+	"interests": ["Cooking", "Gardening"]
+}
+"""
